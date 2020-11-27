940ddc3c20451ce05f2efad21a486ecd2286ce47
Chloe Kelly <chloetay3@gmail.com>
2020-11-27 17:58:17 -0500

    Add blue h3

--- dom-js-exercise-one.js
+++ dom-js-exercise-one.js
@@ -5,3 +5,10 @@ paragraph.textContent = `Hey I'm red!`;
 paragraph.style.color = 'red';
 
 container.appendChild(paragraph);
+
+const heading = document.createElement('h3');
+heading.textContent = `I'm a blue h3!`;
+heading.style.color = 'blue';
+
+container.appendChild(heading);
+
